c09686d76eb042fde5140f50f55b5419741cb643
Casey Watson <watsoncj@gmail.com>
2011-01-26 00:04:21 -0700

    better handing of multiple change sets. still not quite there yet though
    styling

--- dashboard.js
+++ dashboard.js
@@ -5,14 +5,20 @@ var fs = require('fs')
 var xml = require('./lib/node-xml');
 var Mustache = require('./lib/mustache');
 
-var userEmail = {'hjerger': 'watsoncj@gmail.com', 'jeff': 'jstano@unifocus.com'};
+var userEmail = {
+	'cwatson': 'watsoncj@gmail.com', 
+	'hjerger': 'hjerger@unifocus.com', 
+	'jeff': 'jstano@unifocus.com',
+	'belder': 'belder@unifocus.com'
+};
 
 var headers = {'host': 'den-dev-01', 'Authorization': 'Basic Y3dhdHNvbjpMb2NrbncwZA==', 'Accept': 'application/json'};
-var client = http.createClient(8111, 'den-dev-01');
+//var client = http.createClient(8111, 'den-dev-01');
+var client = http.createClient(33333, 'localhost');
 
 http.createServer(function (req, res) {
    res.writeHead(200, {'Content-Type': 'text/plain'});
-   var request = client.request('GET', '/httpAuth/app/rest/builds/?locator=id:bt2', headers);
+   var request = client.request('GET', '/httpAuth/app/rest/builds/?buildType=id:bt2', headers);
    request.end();
    handleResponse(request, function(content) {
       var build = JSON.parse(content).build[0];
@@ -24,63 +30,63 @@ http.createServer(function (req, res) {
       {
          var buildData = JSON.parse(content);
          var buildName = buildData.buildType.name;
-         var status = buildData.status;
+         var buildStatus = buildData.status;
          var statusText = buildData.statusText;
          var changes = buildData.changes;
+
          var request = client.request('GET', changes.href, headers);
          request.end();
          handleResponse(request, function(content)
          {
             var changeData = JSON.parse(content);
+            //var changes = changeData.change;
+            var changes = changeData['change'];
+//            var changeCount = changes.count + (changes.count==1 ? ' change' : ' changes');
+            var changeCount = changeData['@count'];
 
             var view = {
                buildName: buildName,
-               buildStatus: status,
+               buildStatus: buildStatus,
                buildStatusText: statusText,
-               changeCount: changes.count + (changes.count==1 ? ' change' : ' changes'),
-               changes: Array()
+               changeCount: changeCount
             };
 
-            var changes = changeData.change;
+            // handle issue where TeamCity doesn't return a single change as an array
             if (!changes.length)
-              changes = Array(changeData.change);
-            sys.puts(changes.length);
-            for(var change = 0; change<changes.length; change++) {
-               var href = changeData.change[change]['@href'];
+              changes = new Array(changeData.change);
+
+            var changeDetails = new Array();
+            for(var i = 0; i<changes.length; i++) {
+               var href = changes[i]['@href'];
                var request = client.request('GET', href, headers);
 
                request.end();
                handleResponse(request, function(content) {
-                  var changeDetails = JSON.parse(content);
-
-                  var email = userEmail[changeDetails.username];
-                  var avatar = '';
+                  var parsedChanges = JSON.parse(content);
+sys.puts(JSON.stringify(parsedChanges));
+                  var email = userEmail[parsedChanges.user.username];
+                  var avatar = 'http://www.gravatar.com/avatar/';
                   if (email) {
                      var md5 = crypto.createHash('md5').update(email).digest('hex');
-                     avatar = 'http://www.gravatar.com/avatar/' + md5;
+                     avatar += md5;
                   } 
-                  var name = changeDetails.name;
+                  var name = parsedChanges.user.name;
                   if(!name)
-                     name =  changeDetails.userName;
+                     name =  parsedChanges.user.username;
 
-                  view.changes[change] = {
+                  // \n chars will break the JSON. We do however want to preserve them
+                  var comment = parsedChanges.comment.replace(/\n/g,'#newline#');
+
+                  changeDetails.push({
                      name: name,
-                     comment: changeDetails.comment,
+                     comment: comment,
                      avatar: avatar
-                  };
+                  });
 
-                  if (change == changes.length - 1) {
-                     fs.readFile("./template.html", function (err, template) {
-                        if (err) throw err;
-                        res.writeHeader(200, {'Content-Type': 'text/html'});
-                        res.write(Mustache.to_html(""+template, view));
-                        res.end();
-                     });
-                  }
-               });
+                  checkComplete(res, view, changeDetails);
+              });
             }
          });
-         // TODO: fetch change details
       });
    });
 }).listen(8080, '127.0.0.1');
@@ -97,5 +103,50 @@ function handleResponse(request, func)
       response.on('end', function() {
          func.call(this, bigChunk);
       });
+   });
+}
+
+function checkComplete(res, view, changeDetails)
+{
+sys.puts(view.changeCount);
+   // if any of the changes in the view are null, then we are still waiting for some of the callbacks to complete
+   for (var c=0; c<view.changeCount; c++) {
+      sys.puts("change " + c + " of " + view.changeCount + " is " + changeDetails[c]);
+      if(!changeDetails[c])
+         return;
+   }
+
+   var n1 = changeDetails[0].name;
+   var c1 = changeDetails[0].comment;
+   var a1 = changeDetails[0].avatar;
+
+   var n2 = changeDetails[1].name;
+   var c2 = changeDetails[1].comment;
+   var a2 = changeDetails[1].avatar;
+
+//   var n3 = changeDetails[2].name;
+//   var c3 = changeDetails[2].comment;
+//   var a3 = changeDetails[2].avatar;
+
+   var finalView = {
+      buildName: view.buildName,
+      buildStatus: view.buildStatus,
+      buildStatusText: view.buildStatusText,
+      changeCount: view.changeCount,
+      changes: [{name: n1, comment: c1, avatar: a1},{name: n2, comment: c2, avatar: a2}],
+      changes2: [{name: 'why',avatar: "http://www.gravatar.com/avatar/cbebdf1cdbb5aefd1a023f524d04364c"},{name: 'dont you' },{name: 'work?'} ]
+   };
+
+   sys.puts();
+   sys.puts(JSON.stringify(view));
+   sys.puts();
+   sys.puts(JSON.stringify(finalView));
+
+   // all callbacks have completed, render view
+   fs.readFile("./template.html", function (err, template) {
+      if (err) throw err;
+      res.writeHeader(200, {'Content-Type': 'text/html'});
+      res.write(Mustache.to_html(""+template, finalView).replace(/#newline#/g,'<br/>'));
+      res.end();
    });
 }
